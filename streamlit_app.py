14baa5f5ffb797e0bbc4ecfa9065b6ea05a94565
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-25 16:06:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+st.title('Dashboard')
+    
 col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
