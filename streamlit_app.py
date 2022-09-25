bfe163b8f2d728e1df90abd74b9258a2334a73a3
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-25 16:43:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ with c1:
     color='temp_max',
     aggregate='median',
     legend=None,
-    height=340,
+    height=345,
     use_container_width=True)
 with c2:
     st.markdown('### Bar chart')
