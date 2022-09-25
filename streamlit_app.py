de4758249ec8311412dd2a9caa4af03cea8e4f37
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-25 16:14:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plost
 
-st.set_page_config(initial_sidebar_state="expanded")
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
