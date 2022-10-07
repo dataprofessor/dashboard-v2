d591aeec782163ce1cd18a8f167f35c982bdf108
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-10-07 14:49:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-st.sidebar.header('Dashboard `version 2')
+st.sidebar.header('Dashboard `version 2`')
 st.sidebar.markdown('''
 Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
 ''')
